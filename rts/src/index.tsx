64657ef451de5730094c07432505fb15d15f7149
gitAmelio <ameliosconnection@gmail.com>
2023-05-31 00:02:42 +0200

    Types Around Done

--- rts/src/index.tsx
+++ rts/src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import UserSearch from './state/UserSearch';
+
+import UserSearch from './refs/UserSearch'
+// import EventComponent from './events/EventComponent';
+// import UserSearch from './state/UserSearch';
 // import GuestList from './state/GuesList';
 // import Parent from './props/Parent';
 
--- rts/src/refs/UserSearch.tsx
+++ rts/src/refs/UserSearch.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 const users = [
     {name: 'Sarah', age: 20},
     {name: 'Alex', age: 20},
     {name: 'Michael', age: 20}
 
-]
+] 
 
 const UserSearch: React.FC = () => {
+    const inputRef = useRef<HTMLInputElement | null>(null);
     const [name, setName] = useState('');
     const [user, setUser] = useState< { name: string, age: number} | undefined >();
     
+    useEffect(() => {
+        if (!inputRef.current) return;
+        
+        inputRef.current.focus()
+    }, []);
+
     const onClick = () => {
         const foundUser = users.find((user) => {
             return user.name === name;
@@ -23,7 +30,7 @@ const UserSearch: React.FC = () => {
     return (
     <div>
         User Search
-        <input value={name} onChange={e => setName(e.target.value)} />
+        <input ref={inputRef} value={name} onChange={e => setName(e.target.value)} />
         <button onClick={onClick}>Find User</button>
         <div>
             {user && `Found a match = { name:${user.name}, age:${user.age} }`}
